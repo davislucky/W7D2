7e0f6be97269f788ae3651c67600a7018b6dd245
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-29 10:43:41 -0700

    Add instances methods, class methods, and validations to User class

--- practice-for-ch-rails-music-app-long-practice-main/Music_App/app/models/user.rb
+++ practice-for-ch-rails-music-app-long-practice-main/Music_App/app/models/user.rb
@@ -1,2 +1,46 @@
 class User < ApplicationRecord
+    before_validation :ensure_session_token
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6}, allow_nil: true
+    attr_reader :password
+
+    def self.find_user_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private 
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
 end
